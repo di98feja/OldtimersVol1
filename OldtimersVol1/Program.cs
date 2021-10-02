61b393681d9a22c7dc10beee682c8ac9986568a5
Barg <janne.fernlof@hotmail.com>
2021-10-02 09:49:05 +0200

    Namechange - scroller

--- OldtimersVol1/Program.cs
+++ OldtimersVol1/Program.cs
@@ -7,7 +7,7 @@ namespace OldtimersVol1
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Demo())
                 game.Run();
         }
     }
